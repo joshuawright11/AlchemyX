6568e041424fc808f7d161b1c0357f8b593bdd8a
Josh Wright <joshuawright11@gmail.com>
2024-06-08 15:16:39 -0500

    messaging

--- AlchemyX/Query/Query+Sort.swift
+++ AlchemyX/Query/Query+Sort.swift
@@ -1,6 +1,9 @@
 extension Query {
     public func sort<L: LosslessStringConvertible>(_ key: KeyPath<R, L>, ascending: Bool = true) -> Self {
-        let field = R.fields[key]!
+        guard let field = R.fields[key] else {
+            preconditionFailure("unable to find field \(key) of type \(L.self) on resource \(R.self)")
+        }
+
         let sort = QueryParameters.Sort(field: field.name, ascending: ascending)
         initialStorage.parameters.sorts.append(sort)
         return self
